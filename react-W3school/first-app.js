3aa236e4b6c7d173155bbf30e4663b8fb564584f
Dipen Luitel <46955586+dipenluitel@users.noreply.github.com>
2024-09-25 11:59:05 +0545

    Create first-app.js

---
+++ react-W3school/first-app.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hurrey!! This is my react first app</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
